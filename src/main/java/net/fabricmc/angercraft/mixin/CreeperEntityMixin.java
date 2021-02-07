4895eb78088da8da841c03c3b75b3a83c5973f1e
Christian <s164150@student.dtu.dk>
2021-02-07 11:45:33 +0100

    Creepers seek shelter when burning

--- src/main/java/net/fabricmc/angercraft/mixin/CreeperEntityMixin.java
+++ src/main/java/net/fabricmc/angercraft/mixin/CreeperEntityMixin.java
@@ -1,16 +1,27 @@
 package net.fabricmc.angercraft.mixin;
 
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ai.goal.AvoidSunlightGoal;
+import net.minecraft.entity.ai.goal.EscapeSunlightGoal;
 import net.minecraft.entity.mob.CreeperEntity;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(CreeperEntity.class)
 public abstract class CreeperEntityMixin extends HostileEntity  {
 
     public CreeperEntityMixin(EntityType<? extends CreeperEntity> entityType, World world) {
         super(entityType, world);
+    }
+
+    @Inject(at = @At("HEAD"), method = "initGoals")
+    protected void initGoals(CallbackInfo info) {
+        this.goalSelector.add(2, new AvoidSunlightGoal(this));
+        this.goalSelector.add(3, new EscapeSunlightGoal(this, 1.0D));
     }
 
     public void tickMovement() {
